b245a5bab998a2df671b47a6567d48f376bbedf8
Your Name <you@example.com>
2021-02-09 10:55:46 -0600

    5 - Fixing up Main

--- src/snackbarApp/Main.java
+++ src/snackbarApp/Main.java
@@ -6,20 +6,22 @@ public class Main
     {
         System.out.println("Welcome to the Snack Bar!");
 
-            Snack s1 = new Snack("Chips", 5, 1.75, 1);
-//         Snack s2 = new Snack("Chocolate Bar", 1.00);
-//         Snack s3 = new Snack("Pretzel", 2.00);
-//         Snack s4 = new Snack("Soda", 2.50);
-//         Snack s5 = new Snack("Water", 2.75);
+
+            Customer  c1 = new Customer("Carl", 84.53);
+            Customer  c2 = new Customer("Jewl", 43.15);
+//         Customer  cus3 = new Customer("Ct. Dracula", 750.00);
+
 
             VendingMachine v1 = new VendingMachine("Food");
-//         VendingMachine v2 = new VendingMachine("Drink");
-//         VendingMachine v3 = new VendingMachine("Office");
+            VendingMachine v2 = new VendingMachine("Drink");
+            VendingMachine v3 = new VendingMachine("Office");
       
-//
-            Customer  cus1 = new Customer("Carl", 200.00);
-//         Customer  cus2 = new Customer("Jewl", 150.00);
-//         Customer  cus3 = new Customer("Ct. Dracula", 750.00);
+
+            Snack s1 = new Snack("Chips", 17, 1.75, v1.getId());
+            Snack s2 = new Snack("Chocolate Bar", 21, 1.00, v1.getId());
+            Snack s3 = new Snack("Pretzel", 28, 2.00, v1.getId());
+            Snack s4 = new Snack("Soda", 17, 2.50, v2.getId());
+            Snack s5 = new Snack("Water", 21, 2.75, v2.getId());
 
 
 //         System.out.println("*** Working with Data\n");
